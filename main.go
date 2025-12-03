039f9b28f66b614ddc14b640ce1cb1bdcb60ad50
test <bafar94@gmail.com   bafar94@gmail.com>
2025-12-03 21:04:25 +0500

    Задание №2

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 
-	_ "github.com/lib/pq" // драйвер PostgreSQL
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	//Postgre поднимаем из docker-compose.yml
+	//docker compose up -d
+
 	dsn := "host=localhost port=5432 user=postgres password=password dbname=app_db sslmode=disable"
 
 	db, err := sql.Open("postgres", dsn)
@@ -22,4 +26,20 @@ func main() {
 	}
 
 	fmt.Println("Подключение успешно!")
+
+	rows, err := db.Query("SELECT id, username FROM users")
+	if err != nil {
+		log.Fatal("query error: ", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var username string
+		if err := rows.Scan(&id, &username); err != nil {
+			log.Fatal("error reading:", err)
+		}
+
+		fmt.Printf("id:%d name:%s\n", id, username)
+	}
 }
