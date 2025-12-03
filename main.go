0c77d721a1d4090eb94408a429d852c68d1de9ea
test <bafar94@gmail.com   bafar94@gmail.com>
2025-12-03 22:04:22 +0500

    Задание №7

--- main.go
+++ main.go
@@ -1,125 +1,43 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
 )
 
+type User struct {
+	ID       uint `gorm:"primaryKey"`
+	Username string
+	Age      uint
+}
+
 func main() {
-
-	//Postgre поднимаем из docker-compose.yml
-	//docker compose up -d
-
+	// Задание №7
+	//Установим ГОРМ и драйвер для горма
 	dsn := "host=localhost port=5432 user=postgres password=password dbname=app_db sslmode=disable"
 
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("Ошибка открытия базы:", err)
-	}
-	defer db.Close()
-
-	if err := db.Ping(); err != nil {
-		log.Fatal("Ошибка подключения к базе:", err)
-	}
-
-	fmt.Println("Подключение успешно!")
-
-	//	Задание №2
-	//rows, err := db.Query("SELECT id, username FROM users")
-	//if err != nil {
-	//	log.Fatal("query error: ", err)
-	//}
-	//defer rows.Close()
-	//
-	//for rows.Next() {
-	//	var id int
-	//	var username string
-	//	if err := rows.Scan(&id, &username); err != nil {
-	//		log.Fatal("error reading:", err)
-	//	}
-	//
-	//	fmt.Printf("id:%d name:%s\n", id, username)
-	//}
-
-	//Задание №3
-	//username := "Alex"
-	//rows, err := db.Query("SELECT id FROM users WHERE username = $1", username)
-	//if err != nil {
-	//	log.Fatal("query error: ", err)
-	//}
-	//defer rows.Close()
-	//fmt.Printf("user with username: %s\n", username)
-	//for rows.Next() {
-	//	var id int
-	//
-	//	if err := rows.Scan(&id); err != nil {
-	//		log.Fatal("error reading:", err)
-	//	}
-	//	fmt.Printf("id:%d name:%s\n", id, username)
-	//}
-
-	// Задание №4
-	//	newUser := "Gregory"
-	//	var newId int
-	//	err = db.QueryRow(`
-	//        INSERT INTO users (username) VALUES ($1)
-	//        RETURNING id`,
-	//		newUser).Scan(&newId)
-	//
-	//	if err != nil {
-	//		log.Fatal("Error inserting:", err)
-	//	}
-	//	fmt.Printf("Добавлен: %s (ID: %d)\n", newUser, newId)
-	//
-
-	// Задание №5:
-	//newAge := 25
-	//result, err := db.Exec(`
-	//    UPDATE users
-	//    SET age = $1
-	//    WHERE username = $2`,
-	//	newAge, "Alex")
-	//
-	//if err != nil {
-	//	log.Fatal("Error update:", err)
-	//}
-	//
-	//rowsAffected, err := result.RowsAffected()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//fmt.Printf("Updated rows: %d\nAlex is now %d \n", rowsAffected, newAge)
-
-	// Задание №6
-	newUser := "VovaTest"
-	tx, err := db.Begin()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var createdId int
-	err = tx.QueryRow(`
-    INSERT INTO users (username, age) VALUES ($1, $2) 
-    RETURNING id`,
-		newUser, 22).Scan(&createdId)
-	if err != nil {
-		tx.Rollback()
-		log.Fatal(err)
-	}
+	db.AutoMigrate(&User{})
 
-	_, err = tx.Exec("DELETE FROM users WHERE id = $1", createdId)
-	if err != nil {
-		tx.Rollback()
-		log.Fatal(err)
-	}
+	newUser := User{Username: "VovaGORM", Age: 30}
+	db.Create(&newUser)
+	fmt.Printf("User %s created with ID %v and age %d", newUser.Username, newUser.ID, newUser.Age)
 
-	if err := tx.Commit(); err != nil {
-		log.Fatal("error commit:", err)
-	}
+	var user User
+	db.First(&user, "username = ?", "VovaGORM")
+	fmt.Printf("\nUser %s found with ID %v", user.Username, user.ID)
 
-	fmt.Printf("transaction is done! %s (ID: %d) created and deleted in one tx\n", newUser, createdId)
+	user.Age = 40
+	db.Save(&user)
+	fmt.Printf("\nUser %s updated to age %d", user.Username, user.Age)
+
+	db.Delete(&user)
+	fmt.Printf("\nUser %s deleted", user.Username)
 }
