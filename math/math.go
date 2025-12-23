c95d1fd5fe0bbe4b92d99739124c6cfc42bf377d
tla <tla@maynitek.ru>
2025-12-23 10:39:22 +0500

    Задание №2

--- math/math.go
+++ math/math.go
@@ -1,5 +1,14 @@
 package math
 
+import "fmt"
+
 func Sum(a int, b int) int {
 	return a + b
+}
+
+func SumOnlyNatural(a int, b int) int {
+	if a < 0 || b < 0 {
+		fmt.Errorf("%d and %d must be non-negative", a, b)
+	}
+	return (a + b)
 }
--- math/math_test.go
+++ math/math_test.go
@@ -2,15 +2,31 @@ package math
 
 import "testing"
 
-func TestSum(t *testing.T) {
-	result := Sum(1, 2)
-	if result != 3 {
-		t.Errorf("Sum(1, 2) = %d; want 3", result)
-	}
-}
+//Задание №1
+//func TestSum(t *testing.T) {
+//	result := Sum(1, 2)
+//	if result != 3 {
+//		t.Errorf("Sum(1, 2) = %d; want 3", result)
+//	}
+//}
 
 //PS C:\Users\tla\GolandProjects\Learning_EM\math> go test -v
 //=== RUN   TestSum
 //--- PASS: TestSum (0.00s)
 //PASS
 //ok      github.com/UberionAI/Learning_EM/math   0.128s
+
+// Задание №2
+func TestSumOnlyNatural(t *testing.T) {
+	result := SumOnlyNatural(1, -2)
+	if result == 0 {
+		t.Errorf("SumOnlyNatural failed")
+	}
+}
+
+//PS C:\Users\tla\GolandProjects\Learning_EM\math> go test -v
+//=== RUN   TestSumOnlyNatural
+//--- PASS: TestSumOnlyNatural (0.00s)
+//PASS
+//ok      github.com/UberionAI/Learning_EM/math   0.128s
+//PS C:\Users\tla\GolandProjects\Learning_EM\math>
