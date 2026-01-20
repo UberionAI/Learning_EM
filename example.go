ad8d1a551b85ba7ae6a95df811efb8c3fafc441a
tla <tla@maynitek.ru>
2026-01-20 15:09:42 +0500

    Задание №2

--- example.go
+++ example.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	a := 1
-	b := 2
-	c := a + b
-	fmt.Println("Sum:", c)
+	name := "Semen"
+	//a := 1
+	//b := 2
+	//c := a + b
+	//fmt.Println("Sum:", c)
+	fmt.Printf("age: %s\n", name)
 }
 
+//Задание №1
 // go fmt .\example.go
+
+//Задание №2
+//go vet ./...
+//# Learning_EM
+//# [Learning_EM]
+//.\example.go:11:2: fmt.Printf format %d has arg name of wrong type string
+//после исправления
+//go vet ./...
+//пусто...
