ad99670b431106c11cf02c581747ce1b30d3517e
tla <tla@maynitek.ru>
2026-01-20 16:35:35 +0500

    Задание №10 (good test)

--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("usage: app <name>")
-		os.Exit(1)
-	}
-
-	name := os.Args[1]
-	fmt.Println("hello,", name)
+	x := 10
+	fmt.Println(x)
 }
 
-//Задание 9
-//module Learning_EM
+//Задание №10
+//создаём в /GolandProjects/Learning_EM/.git/hooks файл pre-commit
+//#!/bin/sh
 //
-//go 1.22
+//echo "running go test..."
+//go test ./...
+//if [ $? -ne 0 ]; then
+//  echo "go test failed, aborting commit"
+//  exit 1
+//fi
 //
-//require (
-//	github.com/google/uuid v1.6.0
-//)
-//пишем go mod tidy для удаления ненужной зависимости из go.mod
-//стало:
-//module Learning_EM
+//echo "running golangci-lint..."
+//golangci-lint run ./...
+//if [ $? -ne 0 ]; then
+//  echo "golangci-lint failed, aborting commit"
+//  exit 1
+//fi
 //
-//go 1.22
+//echo "pre-commit checks passed"
+//chmod +x .git/hooks/pre-commit
+//
+//
+//теперь при комитах прогоняется go test ./... golangci-lint run ./...
+//если ошибка - откат попытки комита
+//No files committed, 1 file failed to commit: Задание №10 (first test) running go test... # Learning_EM [Learning_EM.test] .\main.go:4:2: declared and not used: x FAIL Learning_EM [build failed] FAIL go test failed, aborting commit
+//исправляем ошибку и теперь норм коммитится
