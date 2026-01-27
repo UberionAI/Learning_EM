ad11d54e2707fdd53d37d5a2a970c51787268c2b
tla <tla@maynitek.ru>
2026-01-27 10:30:04 +0500

    Задание №8

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
-	City string
+func ChangeValue(ptr *int) {
+	*ptr = 100
 }
 
 func main() {
-	p := Person{Name: "Vova", Age: 18, City: "New York"}
-	fmt.Println(p)
+	a := 10
+	fmt.Println("До:", a)
+	ChangeValue(&a)
+	fmt.Println("Новое значение:", a)
 }
