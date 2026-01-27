dc7dd699553529cd7c51a73d5bf3307ef0ba2c97
tla <tla@maynitek.ru>
2026-01-27 09:45:27 +0500

    Задание №2

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var a int
+	var b string
+	var c bool
+
+	fmt.Printf("a = %d is integer\nb = %s is string\nc = %t is bool", a, b, c)
 }
