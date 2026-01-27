65d14b8bed595a5646d1775f466f3dadbc8fd894
tla <tla@maynitek.ru>
2026-01-27 09:49:57 +0500

    Задание №3

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a int
-	var b string
-	var c bool
-
-	fmt.Printf("a = %d is integer\nb = %s is string\nc = %t is bool", a, b, c)
+	a := 50
+	if a%2 == 0 {
+		fmt.Printf("%d is even\n", a)
+	} else {
+		fmt.Printf("%d is odd\n", a)
+	}
 }
