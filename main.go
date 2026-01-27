21cf0e198c47bcf3b0f25fb4b3b12f683ae3d720
tla <tla@maynitek.ru>
2026-01-27 10:14:21 +0500

    Задание №4

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := 50
-	if a%2 == 0 {
-		fmt.Printf("%d is even\n", a)
-	} else {
-		fmt.Printf("%d is odd\n", a)
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
 	}
 }
