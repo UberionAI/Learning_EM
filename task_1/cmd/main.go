14872f3d5aff60d1b324c751d62ad901fc967ca5
test <bafar94@gmail.com   bafar94@gmail.com>
2026-03-12 20:50:56 +0500

    task#1

---
+++ task_1/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"Learning_EM/task_1/internal/handlers"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/users", handlers.UsersHandler)
+	http.HandleFunc("/users/", handlers.UsersHandler)
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ task_1/internal/handlers/handlers.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"Learning_EM/task_1/internal/models"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+var users = make(map[string]models.User)
+
+func UsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case http.MethodPost:
+		registerUser(w, r)
+	case http.MethodGet:
+		listUsers(w, r)
+	case http.MethodPut, http.MethodDelete:
+		updateOrDeleteUser(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	var u models.User
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	id := strconv.Itoa(len(users) + 1)
+	u.ID = id
+	users[id] = u
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(u)
+}
+
+func listUsers(w http.ResponseWriter, r *http.Request) {
+	list := make([]models.User, 0, len(users))
+	for _, u := range users {
+		list = append(list, u)
+	}
+	json.NewEncoder(w).Encode(list)
+}
+
+func updateOrDeleteUser(w http.ResponseWriter, r *http.Request) {
+	path := strings.Trim(r.URL.Path, "/")
+	parts := strings.Split(path, "/")
+
+	if len(parts) != 2 || parts[0] != "users" {
+		http.Error(w, "wrong path", http.StatusBadRequest)
+		return
+	}
+	id := parts[1]
+
+	if _, exists := users[id]; !exists && r.Method == http.MethodPut {
+		http.Error(w, "user is not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		var u models.User
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			http.Error(w, "JSON error!", http.StatusBadRequest)
+			return
+		}
+		u.ID = id
+		users[id] = u
+		json.NewEncoder(w).Encode(u)
+		return
+	}
+
+	delete(users, id)
+	w.WriteHeader(http.StatusNoContent)
+}
